9ed8f2895ff9c7455e71dac38a19c38ff3baed7e
wouterlemaire120@hotmail.com <wouterlemaire120@hotmail.com>
2020-08-18 22:00:18 +0000

    example

---
+++ AppOnly/app/UIAppOnly/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"be/wl/UIAppOnly/test/unit/controller/app.controller"
+], function () {
+	"use strict";
+});
---
+++ DemoService/app/DemoApp/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"be/wl/DemoApp/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
